066907610a9efc1580c8e76983071c10dd98ac4e
JamesonHuang <huangruohai2010@gmail.com>
2015-03-06 13:23:50 +0800

    add sort

---
+++ 1_5.Sort/main_test.cpp
@@ -0,0 +1,23 @@
+/*************************************************************************
+	> File Name:        main_test.cpp
+	> Description:      
+	> Conclusion:          
+	> Author:           rh_Jameson
+	> Created Time:     2015年03月06日 星期五 13时23分05秒
+ ************************************************************************/
+
+#include<iostream>
+#include<string>
+#include<algorithm>
+//#include "Solution.h"
+
+using namespace std;
+
+int main()
+{
+     //Solution solution;
+     //Error test cases from leetcode.com
+     //Test cases
+
+    return 0;
+}
