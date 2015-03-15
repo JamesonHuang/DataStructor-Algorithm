272ae0eb380ac1ee34df276c4b93107726fbdeca
JamesonHuang <462430725@qq.com>
2015-03-15 19:33:28 +0800

    str_opt lib_func achieve

---
+++ 1_3_串/StrOpt/StrOpt.h
@@ -0,0 +1,148 @@
+/*************************************************************************
+    > File Name:        StrOpt.h
+    > Description:      
+    > Conclusion:          
+    > Author:           rh_Jameson
+    > Created Time:     2015年03月15日 星期日 10时48分41秒
+ ************************************************************************/
+
+#ifndef _STROPT_H
+#define _STROPT_H
+
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<climits>
+#include<cassert>
+//#include<cmem>
+using namespace std;
+
+class StrOpt {
+public:
+    /* 字符串转整型 */
+    int atoi(const string s){
+        int len = s.size(), iter = 0;   //串长 & 游标
+        int num = 0, sign = 1;      //整型数字 & 符号位
+        //判空
+        if(s.empty() || s[iter] == '\0')    return 0;
+        //跳过空格
+        while(s[iter] == ' ' && iter < len) ++iter;
+        //判断正负
+        if(s[iter]== '+')   ++iter;
+        if(s[iter] == '-'){
+            sign = -1;
+            ++iter;
+        }
+        for( ; iter < len; ++iter){
+            //跳过中间可能出现的空格
+            if(s[iter] == ' ')  ++iter;
+            //屏蔽0-9以外的字符
+            if(s[iter] < '0' || s[iter] > '9')  break;
+            //溢出处理
+            if(num > INT_MAX / 10 || 
+               (num == INT_MAX / 10 && (s[iter] - '0') > INT_MAX % 10)
+              ){
+                  return sign == -1 ? INT_MIN : INT_MAX;
+              }
+            num = num * 10 + (s[iter] - '0');
+        }
+        return num * sign;
+    }
+
+    /* 整型转字符串 */
+    void itoa(int num, string &str){
+        int sign = num, iter = 0;   //符号标志 & 游标
+        //如传入整数是负数，则先做abs()
+        if(sign < 0){
+            num = -num;
+        }
+        //逐位转换
+        do{
+            str[iter++] = num % 10 + '0';
+            num /= 10;
+        }while(num > 0);
+        //如传入整数是负数，加上'-'        
+        if(sign < 0){
+            str[iter++] = '-';
+        }
+        str[iter] = '\0';       //添加结束符
+        reverse(str, 0, iter - 1);
+    }
+    //逆置字符串
+    void reverse(string &str, int first, int last){
+        while(first < last){
+            swap(str[first++], str[last--]);
+        }
+    }
+    //交换字符
+    void swap(char &a, char &b){
+        char tmp = a;
+        a = b;
+        b = tmp;
+    }
+
+    /* 字符串拷贝 */
+    void StrCpy(string &src, string &dest){
+        //保证src,dest不为空
+        assert(!src.empty() && !dest.empty());
+        int iter = 0, dest_len = dest.size();
+        //只要目标串或源串有一个到了尾部，循环结束
+        while(iter < dest_len && (dest[iter] = src[iter]) != '\0'){
+            ++iter;
+        }
+    }
+    
+    /* 字符串拷贝 */
+    void StrNCpy(string &src, string &dest,int n){
+        //保证src,dest不为空
+        assert(!src.empty() && !dest.empty());
+        //指定长度不能大于源串长度
+        assert(n <= src.size());
+        int iter = 0, dest_len = dest.size();
+        //指定长度如大于目标串长度的处理
+        if(n > dest_len)
+            n = dest_len;
+        //只要到达指定长度或源串到了尾部，循环结束
+        while(iter < n && (dest[iter] = src[iter]) != '\0'){
+            ++iter;
+        }
+        //结束符处理
+        if(dest[iter] != '\0')
+            dest[iter] = '\0';
+    }
+
+    /* 内存拷贝 */
+    void* MemCpy(void *dest, void *src, int len){
+        //转char*,使每次移动都是一个字节
+        char *tmp_dest = (char *)dest;
+        char *tmp_src = (char *)src;
+        int iter = 0;
+        //内存地址不重叠或目标串高位地址重叠源串低位地址时
+        if(tmp_dest < tmp_src || tmp_dest > (tmp_src + len - 1)){
+            while(iter++ < len)
+                *tmp_dest++ = *tmp_src++;
+        }
+        else{   //源串低位地址重叠目标串高位地址时
+            tmp_dest += len;
+            tmp_src += len;
+            iter = len;
+            while(iter > 0){
+                *tmp_dest-- = *tmp_src--;
+            }
+        }
+    }
+    /* 内存初始化 */
+    void *memset(void *buffer,int c, size_t size){
+        assert(buffer!=NULL);  
+        //转char*,逐字节初始化
+        char* ptr = (char *)buffer;
+        while(size-- > 0){
+            *ptr++ = (char)c;
+        }
+        return buffer;
+    }
+private:
+
+};
+
+#endif
---
+++ 1_3_串/StrOpt/main_test.cpp
@@ -0,0 +1,75 @@
+/*************************************************************************
+	> File Name:        main_test.cpp
+	> Description:      
+	> Conclusion:          
+	> Author:           rh_Jameson
+	> Created Time:     2015年03月15日 星期日 10时47分44秒
+ ************************************************************************/
+
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include "StrOpt.h"
+
+using namespace std;
+
+int main()
+{
+
+    /* memset test cases: */
+    char ch[10];
+    StrOpt str_opt;
+    str_opt.memset(ch, '1',10 * sizeof(char));
+    cout << ch << endl;
+    /*
+    int a[10];
+    StrOpt str_opt;
+    str_opt.memset(a, 0, 10 * sizeof(int));
+    for(int i = 0; i < 10; ++i){
+        cout << a[i] << "\t";
+    }
+    */
+    /* strcpy & strncpy memcpy test case: 
+    
+    //string src = "testt dl";
+    //string dest = "hello world";
+    //string src,dest;
+    char dest[] = "testt dl";
+    char src[] = "hello world";
+    StrOpt str_opt; 
+    //str_opt.StrCpy(src, dest);
+    //str_opt.StrNCpy(src, dest, 4);
+    str_opt.MemCpy(dest, src, 7);
+    cout << dest << endl;
+    */
+    
+    /*itoa test cases: 
+    int res;
+    string str = "initial string";
+    StrOpt str_opt;
+    res = 45654;
+    str_opt.itoa(res, str);
+    cout << str << endl;
+    */
+
+    /* atoi test case: 
+    res = str_opt.atoi(str);
+    cout << res << endl;
+    
+    str = "-123";
+    res = str_opt.atoi(str);
+    cout << res << endl;
+    
+    str = "    -3 21adf";
+    res = str_opt.atoi(str);
+    cout << res << endl;
+
+    str = "9999999999999999999999";
+    res = str_opt.atoi(str);
+    cout << res << endl;
+    */
+    
+    
+
+    return 0;
+}
